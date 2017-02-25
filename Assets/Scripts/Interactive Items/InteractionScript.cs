94979a28ce94429edc0de038624fb0e2a092ff0a
Aimé Yamamoto <aimeyamamoto@gmail.com>
2017-02-24 19:04:37 -0500

    Interactive Item + Dynamic Menu
    Interactive Speaker prefab
    Action Menu prefab
    + scripts

---
+++ Assets/Scripts/Interactive Items/InteractionScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionScript : MonoBehaviour {
+
+    private bool isClicked = false;
+
+	// Use this for initialization
+	void Start () {
+        isClicked = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown() {
+        isClicked = !isClicked;
+        if (isClicked) {
+            Debug.Log("Clicked and opening menu");
+        }
+        else {
+            Debug.Log("Closing Menu");
+        };
+        transform.GetChild(0).gameObject.SetActive(isClicked);
+        
+    }
+    
+}
+
+
