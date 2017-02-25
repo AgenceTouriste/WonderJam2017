624270633a7026a251cddb4e86f16959c395777d
Aimé Yamamoto <aimeyamamoto@gmail.com>
2017-02-24 20:11:41 -0500

    Better Ergonomy
    Hold and release action + visual effect
    Disable menu when too far from object

--- Assets/Scripts/Interactive Items/InteractionScript.cs
+++ Assets/Scripts/Interactive Items/InteractionScript.cs
@@ -1,31 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractionScript : MonoBehaviour {
 
-    private bool isClicked = false;
+
+    public float InteractivityRange = 1000;
 
 	// Use this for initialization
 	void Start () {
-        isClicked = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (transform.GetChild(0).gameObject.activeSelf)
+        {
+            float distanceToCenter = Vector2.Distance((Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition), (Vector2) transform.position);
+            if (distanceToCenter > InteractivityRange) {
+                transform.GetChild(0).gameObject.SetActive(false);
+            }
+            Button[] Buttons = this.GetComponentsInChildren<Button>();
+            float old_distance = InteractivityRange;
+            int i = 0;
+            int j = 0;
+            while (i < Buttons.Length)
+            {
+                float distance = Vector2.Distance((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition), (Vector2)Buttons[i].gameObject.GetComponent<Transform>().position);
+                if (distance < old_distance)
+                {
+                    old_distance = distance;
+                    j = i;
+                };
+                i++;
+            }
+            for (int k = 0; k < Buttons.Length; k++) {
+                if (k == j)
+                {
+                    Buttons[k].gameObject.GetComponent<Image>().color = Color.green;
+                }
+                else {
+                    Buttons[k].gameObject.GetComponent<Image>().color = Color.white;
+                }
+            }
+            
 
-    void OnMouseDown() {
-        isClicked = !isClicked;
-        if (isClicked) {
-            Debug.Log("Clicked and opening menu");
         }
-        else {
-            Debug.Log("Closing Menu");
-        };
-        transform.GetChild(0).gameObject.SetActive(isClicked);
-        
+    }
+ 
+    /// <summary>
+    /// Open the actions menu when holding the left click button on the object
+    /// </summary>
+    void OnMouseDown() {
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+    
+
+    /// <summary>
+    /// Close the actions menu when letting go of the left click button
+    /// </summary>
+    void OnMouseUp() {
+        transform.GetChild(0).gameObject.SetActive(false);
     }
     
 }
