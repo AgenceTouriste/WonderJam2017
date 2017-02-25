7922889db05df8ed87a643f2df9d7926cca2ce43
Moonvibes <antoine.ruffion@gmail.com>
2017-02-24 20:31:12 -0500

    Field of view
    Ajout d'un ennemi basique avec un field of view base sur le polygon collider de l'ennemi

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public void Start()
+    {
+        GetComponent<SpriteRenderer>().material.color = Color.green;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponent<SpriteRenderer>().material.color = Color.red;
+        Debug.Log("Red");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GetComponent<SpriteRenderer>().material.color = Color.green;
+        Debug.Log("Green");
+    }
+
+}
