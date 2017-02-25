2ef073a6288ce4ebc2acb3a8ad5bad8f9dd127a4
SatyR91 <scheer.gaetan@gmail.com>
2017-02-25 02:17:04 -0500

    Add player mov (#8)

---
+++ Assets/PlayerMovements.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    //Inspector Variables
+    public float speed = 6.0F;
+    public float maxSpeed = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        MoveForward();
+
+    }
+
+    public void MoveForward()
+    {
+
+        if (Input.GetKey("up"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(0, speed * Time.fixedDeltaTime, 0);
+        }
+        if (Input.GetKey("down"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(0, -speed * Time.fixedDeltaTime, 0);
+        }
+        if (Input.GetKey("right"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(speed * Time.fixedDeltaTime, 0, 0);
+        }
+        if (Input.GetKey("left"))//Press up arrow key to move forward on the Y AXIS
+        {
+            transform.Translate(-speed * Time.fixedDeltaTime, 0, 0);
+        }
+        
+
+    }
+
+}
