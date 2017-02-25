a12aeb67b9aff1f256987b3c18b9357e08b2e6a2
Aimé Yamamoto <aimeyamamoto@gmail.com>
2017-02-24 20:31:20 -0500

    Even better ergonomy
    Deadzone in the center and Interactivity Range for the borders

--- Assets/Scripts/Interactive Items/InteractionScript.cs
+++ Assets/Scripts/Interactive Items/InteractionScript.cs
@@ -3,66 +3,100 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InteractionScript : MonoBehaviour {
+public class InteractionScript : MonoBehaviour
+{
 
 
     public float InteractivityRange = 1000;
+    public float DeadzoneRange = 100;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (transform.GetChild(0).gameObject.activeSelf)
         {
-            float distanceToCenter = Vector2.Distance((Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition), (Vector2) transform.position);
-            if (distanceToCenter > InteractivityRange) {
-                transform.GetChild(0).gameObject.SetActive(false);
-            }
             Button[] Buttons = this.GetComponentsInChildren<Button>();
-            float old_distance = InteractivityRange;
-            int i = 0;
-            int j = 0;
-            while (i < Buttons.Length)
+
+            float distanceToCenter = Vector2.Distance((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition), (Vector2)transform.position);
+
+            if (distanceToCenter > InteractivityRange)
             {
-                float distance = Vector2.Distance((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition), (Vector2)Buttons[i].gameObject.GetComponent<Transform>().position);
-                if (distance < old_distance)
-                {
-                    old_distance = distance;
-                    j = i;
-                };
-                i++;
+                CloseMenu();
             }
-            for (int k = 0; k < Buttons.Length; k++) {
-                if (k == j)
+            else if (distanceToCenter > DeadzoneRange)
+            {
+                float old_distance = InteractivityRange;
+                int i = 0;
+                int j = 0;
+                while (i < Buttons.Length)
                 {
-                    Buttons[k].gameObject.GetComponent<Image>().color = Color.green;
+                    float distance = Vector2.Distance((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition), (Vector2)Buttons[i].gameObject.GetComponent<Transform>().position);
+                    if (distance < old_distance)
+                    {
+                        old_distance = distance;
+                        j = i;
+                    };
+                    i++;
                 }
-                else {
+                for (int k = 0; k < Buttons.Length; k++)
+                {
+                    if (k == j)
+                    {
+                        Buttons[k].gameObject.GetComponent<Image>().color = Color.green;
+                    }
+                    else
+                    {
+                        Buttons[k].gameObject.GetComponent<Image>().color = Color.white;
+                    }
+                }
+
+            }
+            else
+            {
+                for (int k = 0; k < Buttons.Length; k++)
+                {
                     Buttons[k].gameObject.GetComponent<Image>().color = Color.white;
                 }
             }
-            
-
         }
     }
- 
+
+
+    /// <summary>
+    /// Close the menu and reset the buttons with default colors
+    /// </summary>
+    void CloseMenu()
+    {
+        Button[] Buttons = this.GetComponentsInChildren<Button>();
+        for (int k = 0; k < Buttons.Length; k++)
+        {
+            Buttons[k].gameObject.GetComponent<Image>().color = Color.white;
+        }
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+
     /// <summary>
     /// Open the actions menu when holding the left click button on the object
     /// </summary>
-    void OnMouseDown() {
+    void OnMouseDown()
+    {
         transform.GetChild(0).gameObject.SetActive(true);
     }
-    
+
 
     /// <summary>
     /// Close the actions menu when letting go of the left click button
     /// </summary>
-    void OnMouseUp() {
-        transform.GetChild(0).gameObject.SetActive(false);
+    void OnMouseUp()
+    {
+        CloseMenu();
     }
-    
+
 }
 
 
