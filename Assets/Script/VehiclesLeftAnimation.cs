7a9a1f33bd50fcd657545555107355a3199e3f44
Aimé Yamamoto <aimeyamamoto@gmail.com>
2017-02-25 23:26:23 -0500

    Intro level + model 3d maya speaker (#14)

---
+++ Assets/Script/VehiclesLeftAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehiclesLeftAnimation : MonoBehaviour
+{
+
+    public int vehiclespeed;
+    public int maxdistance;
+    public Vector3 spawnPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < maxdistance)
+        {
+            transform.position = spawnPoint;
+
+        }
+        else
+        {
+            transform.Translate(new Vector3(-vehiclespeed, 0, 0));
+        }
+    }
+}
